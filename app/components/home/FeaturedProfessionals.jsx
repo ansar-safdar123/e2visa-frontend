e7de5be3f72e19b876e6e80f55414e4e5316c487
ansar-safdar123 <71287779+ansar-safdar123@users.noreply.github.com>
2025-05-24 11:18:04 +0500

    update

--- app/components/home/FeaturedProfessionals.jsx
+++ app/components/home/FeaturedProfessionals.jsx
@@ -3,6 +3,8 @@
 import Image from "next/image";
 import React from "react";
 import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const professionals = [
   {
