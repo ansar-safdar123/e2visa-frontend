441e0940126fdb67e07f72ee437e01df75fdca56
ansar-safdar123 <71287779+ansar-safdar123@users.noreply.github.com>
2025-05-29 00:21:43 +0500

    add contact page

---
+++ app/contact/page.jsx
@@ -0,0 +1,200 @@
+'use client';
+
+import Link from 'next/link';
+import Image from 'next/image';
+import { useState } from 'react';
+
+export default function Contact() {
+    const [formData, setFormData] = useState({
+        fullName: '',
+        email: '',
+        subject: '',
+        message: ''
+      });
+
+  return (
+    <div className="min-h-screen">
+      
+
+      {/* Contact Section */}
+      <div className="">
+        <div className="grid grid-cols-1 md:grid-cols-2 ">
+          {/* Left Side - Contact Information */}
+          <div className="p-8 flex flex-col items-center justify-center !text-white bg-[url('/images/auth/signin/signinImg.png')] bg-cover bg-center relative ">
+            <div className="absolute inset-0 bg-[#1B263B99] z-0" />
+            <div className="relative w-[430px]  z-10 p-8">
+            <h2 className="text-3xl font-semibold mb-6">Contact Information</h2>
+            <p className=" mb-8">Say something to start a live chat!</p>
+
+            <div className="space-y-6">
+              <div className="flex items-center gap-4">
+                <div className="w-10 h-10 flex items-center justify-center bg-[#40433F] rounded-full">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
+                  </svg>
+                </div>
+                <p className="">941.518.7138</p>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <div className="w-10 h-10 flex items-center justify-center bg-[#40433F] rounded-full">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
+                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
+                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
+                  </svg>
+                </div>
+                <p className="">info@e2visa.com</p>
+              </div>
+
+              <div className="flex items-center gap-4">
+                <div className="w-10 h-10 flex items-center justify-center bg-[#40433F] rounded-full">
+                  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" viewBox="0 0 20 20" fill="currentColor">
+                    <path fillRule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clipRule="evenodd" />
+                  </svg>
+                </div>
+                <p className="">9040 Town Center Parkway, Lakewood Ranch, FL 34202</p>
+              </div>
+            </div>
+
+            <div className="mt-8 flex gap-4">
+              <Link href="https://facebook.com" className="w-10 h-10 flex items-center justify-center bg-[#40433F] rounded-full text-white hover:bg-opacity-90 transition-colors">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"/>
+                </svg>
+              </Link>
+              <Link href="https://twitter.com" className="w-10 h-10 flex items-center justify-center bg-[#40433F] rounded-full text-white hover:bg-opacity-90 transition-colors">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24">
+                  <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/>
+                </svg>
+              </Link>
+            </div>
+            </div>
+          </div>
+
+          {/* Right Side - Contact Form */}
+          <div className="p-8 flex flex-col items-center justify-center">
+            <div className="w-[540px]">
+                
+            <h2 className="text-3xl font-semibold mb-8">Get in Touch</h2>
+            <form className="space-y-6">
+             {/* Full Name Input */}
+             <div className="relative">
+              <div className="absolute inset-y-0 left-4 flex items-center pointer-events-none">
+                <Image
+                  src="/images/auth/signin/user.png"
+                  alt="User icon"
+                  width={23}
+                  height={20}
+                />
+              </div>
+              <input
+                type="text"
+                id="fullName"
+                name="fullName"
+                value={formData.fullName}
+                // onChange={handleChange}
+                placeholder="Enter your full name"
+                className="pl-12 w-full pr-4 py-4 rounded-lg border text-[#9E9E9E] font-medium text-[22px] border-[#1B263B] focus:ring-2 focus:ring-[#2EC4B6] focus:border-transparent outline-none"
+              />
+              <label
+                htmlFor="fullName"
+                className="absolute text-sm text-[#1E1E1E] left-12 bg-[#F3F7F9] px-1 -top-2 peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 transition-all"
+              >
+                Full Name
+              </label>
+            </div>
+
+            {/* Email Input */}
+            <div className="relative">
+              <div className="absolute inset-y-0 left-4 flex items-center pointer-events-none">
+                <Image
+                  src="/images/auth/signin/mail.png"
+                  alt="Email icon"
+                  width={23}
+                  height={20}
+                />
+              </div>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.email}
+                // onChange={handleChange}
+                placeholder="email@gmail.com"
+                className="pl-12 w-full pr-4 py-4 rounded-lg border text-[#9E9E9E] font-medium text-[22px] border-[#1B263B] focus:ring-2 focus:ring-[#2EC4B6] focus:border-transparent outline-none"
+              />
+              <label
+                htmlFor="email"
+                className="absolute text-sm text-[#1E1E1E] left-12 bg-[#F3F7F9] px-1 -top-2 peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 transition-all"
+              >
+                Email
+              </label>
+            </div>
+
+            <div className="relative">
+              <div className="absolute inset-y-0 left-4 flex items-center pointer-events-none">
+                <Image
+                  src="/images/auth/signin/mail.png"
+                  alt="Email icon"
+                  width={23}
+                  height={20}
+                />
+              </div>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                value={formData.subject}
+                // onChange={handleChange}
+                placeholder="email@gmail.com"
+                className="pl-12 w-full pr-4 py-4 rounded-lg border text-[#9E9E9E] font-medium text-[22px] border-[#1B263B] focus:ring-2 focus:ring-[#2EC4B6] focus:border-transparent outline-none"
+              />
+              <label
+                htmlFor="email"
+                className="absolute text-sm text-[#1E1E1E] left-12 bg-[#F3F7F9] px-1 -top-2 peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 transition-all"
+              >
+                Subject
+              </label>
+            </div>
+
+            <div className="relative">
+              <div className="absolute inset-y-0 left-4 flex items-center pointer-events-none">
+                <Image
+                  src="/images/auth/signin/mail.png"
+                  alt="Email icon"
+                  width={23}
+                  height={20}
+                />
+              </div>
+              <textarea
+                type="email"
+                id="email"
+                name="email"
+                value={formData.message}
+                // onChange={handleChange}
+                placeholder="email@gmail.com"
+                className="pl-12 w-full pr-4 py-4 rounded-lg border text-[#9E9E9E] font-medium text-[22px] border-[#1B263B] focus:ring-2 focus:ring-[#2EC4B6] focus:border-transparent outline-none"
+              />
+              <label
+                htmlFor="email"
+                className="absolute text-sm text-[#1E1E1E] left-12 bg-[#F3F7F9] px-1 -top-2 peer-placeholder-shown:top-4 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 transition-all"
+              >
+                Message
+              </label>
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-[#0A3161] text-white !mt-14 py-4 2xl:py-5 rounded-lg hover:bg-bg-[#102742] transition-colors font-semibold text-2xl"
+            lassName="w-full bg-[#40433F] text-white py-3 px-6 rounded-lg hover:bg-opacity-90 transition-colors"
+              >
+                Send Message
+              </button>
+            </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+} 
